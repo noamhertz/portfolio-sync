87aebb4ba069a3ae0c1f21012bc0adc3ac28a77c
noamhertz <hertz.noam@gmail.com>
2024-11-05 00:07:59 +0200

    frontend init with chakra snippets

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App";
+import store from "./store";
+import { Provider } from "@/components/ui/provider";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          <App />
+        </Provider>
+      </QueryClientProvider>
+    </ReduxProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
